23c2be4f56903c18979f9a41f53512ebd421bbfd
Jose Perez <jose.perez@vertilux.com>
2022-02-07 16:20:09 -0500

    first commit

---
+++ lib/paycargo.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "paycargo/version"
+
+module Paycargo
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/paycargo/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Paycargo
+  VERSION = "0.1.0"
+end
